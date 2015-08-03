Fixed RF scaling (again) and updated example datasets

generate_test_data.py
@@ -4,10 +4,14 @@ import matplotlib
 import matplotlib.pyplot as plt
 import scipy.io as sio
 
+seq = poet_sim.read_poet_simulation('/Users/hansenms/temp/pulse_seqs/ssfp_ss_startup_store_mag.txt')
+bloch.save_seq(seq,'ssfp_store.p')
+
 seq = poet_sim.read_poet_simulation('/Users/hansenms/temp/pulse_seqs/ssfp_ss_startup.txt')
-seq[:,0] = 6.146890*seq[:,0]
-bloch.save_seq(seq,'ssfp_new.p')
+bloch.save_seq(seq,'ssfp_ss_startup.p')
 
-plt.plot(np.abs(seq[92000:96000,0]))
+seq = poet_sim.read_poet_simulation('/Users/hansenms/temp/pulse_seqs/ssfp_ss_tr10.txt')
+bloch.save_seq(seq,'ssfp_ss_tr10.p')
 
-70.0/(360.0*(np.sum(np.abs(seq[92000:96000,0]),0)*1.0e-6*42.576e6)/(np.pi*2))
+seq = poet_sim.read_poet_simulation('/Users/hansenms/temp/pulse_seqs/flash.txt')
+bloch.save_seq(seq,'flash.p')

poet_sim.py
@@ -39,7 +39,7 @@ def read_poet_simulation(filename):
         for t in range(sequence_table_raw.shape[0]):
             if (sequence_table_raw[t,rf_signal_column] > 0):
                 #TODO: Have to figure out correct RF scaling
-                sequence_table[t,0] = 0.037570*1.0e-6*cmath.rect(sequence_table_raw[t,rf_signal_column], np.pi*((sequence_table_raw[t,rf_phase_column] + sequence_table_raw[t,nco_phase_column])/180.0))
+                sequence_table[t,0] = 0.036755*1.0e-6*cmath.rect(sequence_table_raw[t,rf_signal_column], np.pi*((sequence_table_raw[t,rf_phase_column] + sequence_table_raw[t,nco_phase_column])/180.0))
             sequence_table[t,1] = 1.0e-3*sequence_table_raw[t,z_gradient_column]
             sequence_table[t,2] = 1.0e-3*sequence_table_raw[t,y_gradient_column]
             sequence_table[t,3] = 1.0e-3*sequence_table_raw[t,x_gradient_column]